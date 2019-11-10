c2a62da2e09c7d71b127588e03eec1c940ad7f9a
Mingjiong liao <22457102+mingjiongliao@users.noreply.github.com>
2019-11-10 17:11:24 -0500

    start new project from wangqiang

---
+++ app/src/main/java/com/example/finalproject/ForCurrency.java
@@ -0,0 +1,270 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
+public class ForCurrency extends AppCompatActivity {
+
+    ArrayList<CountryItem> objects = new ArrayList<>( );
+
+    private static int ACTIVITY_VIEW_CONTACT = 33;
+    int positionClicked = 0;
+    MyOwnAdapter myAdapter;
+    private EditText result=null;
+    private String baseValue=null;
+    private String symbols=null;
+//    private String response = "https://api.exchangeratesapi.io/latest?base="+base+"&symbols="+symbols;
+    private ArrayList<CountryItem> mCountryList;
+    private CountryAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_for_currency);
+
+
+        EditText amount = (EditText)findViewById(R.id.input);
+        result = (EditText)findViewById(R.id.result);
+        Button convert = (Button)findViewById(R.id.convert);
+        Button insert = (Button)findViewById(R.id.insert);
+        ListView theList = (ListView)findViewById(R.id.the_list);
+
+
+
+        initList();
+//        Spinner spinnerCountries=findViewById(R.id.spinner);
+        Spinner spinner1 =(Spinner)findViewById(R.id.spinner1);
+        Spinner spinner2 =(Spinner)findViewById(R.id.spinner2);
+        mAdapter =new CountryAdapter(this,mCountryList);
+        spinner1.setAdapter(mAdapter);
+        spinner2.setAdapter(mAdapter);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                CountryItem clickedItem=(CountryItem)parent.getItemAtPosition(position);
+                String clickedCountryName=clickedItem.getmCountryName();
+                Toast.makeText(ForCurrency.this,clickedCountryName+"selected",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                CountryItem clickedItem=(CountryItem)parent.getItemAtPosition(position);
+                String clickedCountryName=clickedItem.getmCountryName();
+                Toast.makeText(ForCurrency.this,clickedCountryName+"selected",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        CurrencyDatabaseOpenHelper dbOpener = new CurrencyDatabaseOpenHelper(this);
+
+        SQLiteDatabase db = dbOpener.getWritableDatabase();
+//        dbOpener.onUpgrade(db,1,2);
+
+        //query all the results from the database:
+        String [] columns = {CurrencyDatabaseOpenHelper.COL_ID, CurrencyDatabaseOpenHelper.COL_MESSAGE, CurrencyDatabaseOpenHelper.COL_FLAG1, CurrencyDatabaseOpenHelper.COL_FLAG2};
+        Log.d("dddd", "onCreate: "+columns[0].toString()+columns[2].toString());
+        Cursor results = db.query(false, CurrencyDatabaseOpenHelper.TABLE_NAME, columns, null, null, null, null, null, null);
+        //find the column indices:
+
+        int mesColIndex = results.getColumnIndex(CurrencyDatabaseOpenHelper.COL_MESSAGE);
+        int idColIndex = results.getColumnIndex(CurrencyDatabaseOpenHelper.COL_ID);
+        int flag1ColIndex=results.getColumnIndex(CurrencyDatabaseOpenHelper.COL_FLAG1);
+        int flag2ColIndex=results.getColumnIndex(CurrencyDatabaseOpenHelper.COL_FLAG2);
+
+        //iterate over the results, return true if there is a next item:
+        while(results.moveToNext())
+        {
+            int flag1=results.getInt(flag1ColIndex);
+            int flag2=results.getInt(flag2ColIndex);
+            String message = results.getString(mesColIndex);
+            long id = results.getLong(idColIndex);
+
+            //add the new Contact to the array list:
+            objects.add(new CountryItem(message,flag1,flag2));
+        }
+
+        myAdapter = new MyOwnAdapter();
+        theList.setAdapter(myAdapter);
+        printCursor(results);
+
+
+        convert.setOnClickListener(v -> {
+            if(amount.getText().toString()==null){
+                //todo ask tutor
+                Toast.makeText(this, "Please enter amount first", Toast.LENGTH_LONG).show();
+            }else{
+                //how to use api to get the rate
+                baseValue =mCountryList.get(spinner1.getSelectedItemPosition()).getmCountryName();
+
+                Log.d("", "onCreate: "+baseValue);
+                symbols= mCountryList.get(spinner2.getSelectedItemPosition()).getmCountryName();
+                String response = "https://api.exchangeratesapi.io/latest?base="+baseValue+"&symbols="+symbols;
+                try{
+                    double value=Double.parseDouble(amount.getText().toString());
+
+                    Log.d("symbol", "onCreate: "+value);
+
+//                    try {
+//                        JSONObject jsonObject=new JSONObject(response);
+//                        Log.d("symbol", "onCreate: "+response);
+//                        JSONObject rateObject=jsonObject.getJSONObject("rates");
+//
+//                        Double rate=rateObject.getDouble(symbols);
+
+                        ImageView imageViewFlag1 =findViewById(R.id.imageView1);
+                        ImageView imageViewFlag2 =findViewById(R.id.imageView2);
+
+                        imageViewFlag1.setImageResource(mCountryList.get(spinner1.getSelectedItemPosition()).getmFlagImage());
+                        imageViewFlag2.setImageResource(mCountryList.get(spinner2.getSelectedItemPosition()).getmFlagImage());
+                        result.setText(value+" "+baseValue+" = "+String.valueOf(3*value)+" "+symbols);
+//                    }catch (JSONException e){
+//                        Snackbar.make(insert, "jason failed", Snackbar.LENGTH_LONG).show();
+//                    }
+
+
+
+                    Toast.makeText(this, "Convert seccessfully!", Toast.LENGTH_LONG).show();
+                }catch(Exception e){
+                    Toast.makeText(this, "Please enter number!", Toast.LENGTH_LONG).show();
+                }
+
+
+
+            }
+        });
+
+        insert.setOnClickListener(v -> {
+            ContentValues newRowValues = new ContentValues();
+            //put string name in the NAME column:
+            ImageView flag1Image=(ImageView)findViewById(R.id.imageView1);
+            ImageView flag2Image=(ImageView)findViewById(R.id.imageView2);
+            int flag1=(int)flag1Image.getTag();
+            Log.d("", "onCreate: "+flag1);
+            int flag2=(int)flag2Image.getTag();
+            newRowValues.put(CurrencyDatabaseOpenHelper.COL_MESSAGE, result.getText().toString());
+            newRowValues.put(CurrencyDatabaseOpenHelper.COL_FLAG1, flag1);
+            newRowValues.put(CurrencyDatabaseOpenHelper.COL_FLAG2, flag2);
+            long newId = db.insert(CurrencyDatabaseOpenHelper.TABLE_NAME, null, newRowValues);
+
+            objects.add(new CountryItem(result.getText().toString(),flag1,flag2));
+            //update the listView:
+            myAdapter.notifyDataSetChanged();
+            Snackbar.make(insert, "Insert sucessfully!", Snackbar.LENGTH_LONG).show();
+        });
+
+        theList.setOnItemClickListener(( parent,  view,  position,  id) -> {
+            Log.d("you clicked on :" , "item "+ position);
+            //save the position in case this object gets deleted or updated
+            positionClicked = position;
+
+            //When you click on a row, open selected contact on a new page (ViewContact)
+            CountryItem chosenOne = objects.get(position);
+            Intent nextPage = new Intent(ForCurrency.this, ViewMessage.class);
+            nextPage.putExtra("Flag1",chosenOne.getmFlagImage());
+            nextPage.putExtra("Flag2",chosenOne.getoFlagImage());
+            nextPage.putExtra("Message", chosenOne.toString());
+            nextPage.putExtra("Id", position);
+            startActivity(nextPage);
+//            startActivityForResult(nextPage, ACTIVITY_VIEW_CONTACT);
+        });
+    }
+    private class MyOwnAdapter extends BaseAdapter {
+
+        public int getCount() {  return objects.size();  } //This function tells how many objects to show
+
+        public CountryItem getItem(int position) { return objects.get(position);  }  //This returns the string at position p
+
+        public long getItemId(int p) { return p; } //This returns the database id of the item at position p
+
+        public View getView(int p, View recycled, ViewGroup parent)
+        {
+
+
+//            LayoutInflater inflater = getLayoutInflater();
+//            View thisRow = null;
+//            thisRow = inflater.inflate(R.layout.row_layout, null);
+//            TextView itemField = thisRow.findViewById(R.id.message);
+//            itemField.setText(getItem(p).toString());
+
+            LayoutInflater inflater = getLayoutInflater();
+
+            View newView = inflater.inflate(R.layout.row_layout, parent, false );
+
+            CountryItem thisRow = getItem(p);
+
+            ImageView flag1 = (ImageView) newView.findViewById(R.id.imageView1);
+            TextView message = (TextView)newView.findViewById(R.id.message);
+            ImageView flag2=(ImageView) newView.findViewById(R.id.imageView2);
+
+            if(thisRow!=null){
+                flag1.setImageResource(thisRow.getmFlagImage());
+                message.setText(thisRow.getmCountryName());
+                flag2.setImageResource(thisRow.getoFlagImage());
+            }
+
+
+            return newView;
+        }
+    }
+
+    public void printCursor( Cursor c){
+        c.moveToFirst();
+
+
+        int mesColIndex = c.getColumnIndex(CurrencyDatabaseOpenHelper.COL_MESSAGE);
+        int idColIndex = c.getColumnIndex(CurrencyDatabaseOpenHelper.COL_ID);
+
+        //iterate over the results, return true if there is a next item:
+        while(c.moveToNext())
+        {
+            String message = c.getString(mesColIndex);
+            long id = c.getLong(idColIndex);
+
+        }
+
+    }
+    private void initList(){
+        mCountryList=new ArrayList<>();
+        mCountryList.add(new CountryItem("USD",R.drawable.usd));
+        mCountryList.add(new CountryItem("CAD",R.drawable.cad));
+        mCountryList.add(new CountryItem("CNY",R.drawable.chy));
+        mCountryList.add(new CountryItem("JPY",R.drawable.jpy));
+        mCountryList.add(new CountryItem("KRW",R.drawable.krw));
+        mCountryList.add(new CountryItem("VND",R.drawable.vnd));
+    }
+
+}
---
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button milestone1Button = (Button)findViewById(R.id.button);
+        milestone1Button.setOnClickListener ( Click ->
+        {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            //This is the builder pattern, just call many functions on the same object:
+            AlertDialog dialog = builder.setTitle("This is information about our team member")
+                    .setMessage("Do you want to go there?")
+                    .setPositiveButton("Yes", (d,w) -> {
+                                Intent student = new Intent(MainActivity.this, StudentInfo.class);
+                                startActivity(student);
+                    }
+
+                    )
+                    //If you click the "Cancel" button:
+                    .setNegativeButton("Cancel", (d,w) -> {  /* nothing */})
+                    .create();
+
+            //then show the dialog
+            dialog.show();
+
+            //show a notification: first parameter is any view on screen. second parameter is the text. Third parameter is the length (SHORT/LONG)
+            Snackbar.make(milestone1Button, "I'm a snackbar", Snackbar.LENGTH_LONG).show();
+
+            //show a notice window to say how many were updated.
+            Toast.makeText(this, "I'm a toast", Toast.LENGTH_LONG).show();
+
+
+
+        });
+
+        Button currency =(Button)findViewById(R.id.currency);
+        currency.setOnClickListener(v -> {
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+
+            //This is the builder pattern, just call many functions on the same object:
+            AlertDialog dialog1 = builder1.setTitle("You will get into currency activity that is created by qing wang")
+                    .setMessage("Are you sure you will get in?")
+                    .setPositiveButton("Yes", (d,w) -> {
+                                Intent goToPage2 = new Intent(MainActivity.this, ForCurrency.class);
+                                startActivity(goToPage2);
+                            }
+
+                    )
+                    //If you click the "Cancel" button:
+                    .setNegativeButton("Cancel", (d,w) -> {  /* nothing */})
+                    .create();
+
+            //then show the dialog
+            dialog1.show();
+
+        });
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "We are processing", Toast.LENGTH_LONG).show();
+    }
+}
