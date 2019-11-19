56bae4444400cb269771589e10d4829da0e3a8f5
algonquinyuanyuan2019 <luo8611yuan@gmail.com>
2019-11-18 23:11:53 -0500

    milestone2_finish_v2

--- app/src/main/java/com/example/finalproject/MainActivity_recipe.java
+++ app/src/main/java/com/example/finalproject/MainActivity_recipe.java
@@ -1,24 +1,20 @@
 package com.example.finalproject;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity_recipe extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.recipe_activity_main);
 
         Button milestone1Button = (Button)findViewById(R.id.searchrecipybutton);
         milestone1Button.setOnClickListener ( Click ->
@@ -27,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 //           AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
             //This is the builder pattern, just call many functions on the same object:
- //           AlertDialog dialog = builder.setTitle("I’m ready for milestone 1")
+ //           AlertDialog recipe_dialog = builder.setTitle("I’m ready for milestone 1")
  //                   .setMessage("Please give bonus marks")
  //                   .setPositiveButton("Yes", (d,w) -> {  /* nothing */}
 
@@ -36,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
   //                  .setNegativeButton("Cancel", (d,w) -> {  /* nothing */})
   //                  .create();
 
-            //then show the dialog
-  //          dialog.show();
+            //then show the recipe_dialog
+  //          recipe_dialog.show();
 
             //show a notification: first parameter is any view on screen. second parameter is the text. Third parameter is the length (SHORT/LONG)
             Snackbar.make(milestone1Button, "I'm a snackbar", Snackbar.LENGTH_LONG).show();
