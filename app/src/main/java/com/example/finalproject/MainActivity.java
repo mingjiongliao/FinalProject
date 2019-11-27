f6798f5d57bb7e33f97e49117082cfbc502a216e
yingzirenfeng <wang0582@algonquinlive.com>
2019-11-27 00:09:30 -0500

    update

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -125,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
         switch(item.getItemId())
         {
             //what to do when the menu item is selected:
-            case R.id.action_sign_out:
+            case R.id.help:
                 Toast.makeText(this, "You clicked on sign out", Toast.LENGTH_LONG).show();
                 break;
             case R.id.option1:
