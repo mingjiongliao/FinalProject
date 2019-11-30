be321df5cf6a09e38ae403ba40bfcf465f9a1ec2
Mingjiong Liao <liao0019@algonquinlive.com>
2019-11-29 22:16:39 -0500

    Update FavoriteDBhelper.java
    update database

--- app/src/main/java/com/example/finalproject/FavoriteDBhelper.java
+++ app/src/main/java/com/example/finalproject/FavoriteDBhelper.java
@@ -2,7 +2,10 @@ package com.example.finalproject;
 
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -15,6 +18,9 @@ public class FavoriteDBhelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "Stations";
     public static final String COL_ID = "_id";
     public static final String COL_TITLE = "_title";
+    public static final String COL_LATITUDE = "_latitude";
+    public static final String COL_LONGTITUDE = "_longtitude";
+    public static final String COL_PHONE = "_phone";
 
     public FavoriteDBhelper(Activity ctx) {
         //The factory parameter should be null, unless you know a lot about Database Memory management
@@ -44,4 +50,39 @@ public class FavoriteDBhelper extends SQLiteOpenHelper {
         //Delete the old table:
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
     }
+
+    //insert data
+    public long insertData(long id, String title, double latitude, double longtitude, String phone ) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_ID, id);
+        contentValues.put(COL_TITLE, title);
+        contentValues.put(COL_LATITUDE, latitude);
+        contentValues.put(COL_LONGTITUDE, longtitude);
+        contentValues.put(COL_PHONE, phone);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        return result ; //if result = -1 data doesn't insert
+    }
+
+    // method to delete a Record
+    public int deleteEntry(int id)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String where="MessageID=?";
+        int numberOFEntriesDeleted= db.delete(TABLE_NAME, where, new String[]{Integer.toString(id)});
+        return numberOFEntriesDeleted;
+    }
+
+    //view data
+    public Cursor viewData(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "Select * from "+TABLE_NAME;
+        Cursor cursor = db.rawQuery(query, null);
+        Log.v("Cursor Object", DatabaseUtils.dumpCursorToString(cursor));
+        return cursor;
+    }
+
 }
