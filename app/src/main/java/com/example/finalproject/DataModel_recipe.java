5e3ff01df8ce9d3ed4781dbfe60362a0a8a4af31
algonquinyuanyuan2019 <luo8611yuan@gmail.com>
2019-12-02 12:55:01 -0500

    final_v9

--- app/src/main/java/com/example/finalproject/DataModel_recipe.java
+++ app/src/main/java/com/example/finalproject/DataModel_recipe.java
@@ -1,5 +1,13 @@
 package com.example.finalproject;
-
+/**
+ * File name: DataModel_recipe.java
+ * Author: Chunyuan Luo, ID# 040926918
+ * Course: 19F_CST2335_010_020 Mobile Graphic interface Prog
+ * Assignment: Final Project
+ * Date: 2019-11-16
+ * Professor: Adewole Adewumi
+ * Purpose:  data model to contain each recipe item
+ */
 public class DataModel_recipe {
 
     private String publisher;
@@ -11,6 +19,17 @@ public class DataModel_recipe {
     private String social_rank;
     private String publisher_url;
 
+    /**
+     * Constructor, used to assign value to class member variables
+     * @param publisher
+     * @param f2f_url
+     * @param title
+     * @param source_url
+     * @param recipe_id
+     * @param image_url
+     * @param social_rank
+     * @param publisher_url
+     */
     //String publisher, String f2f_url, String recipe_title, String source_url, String recipe_id, String image_url, String social_rank, String publisher_url
     public DataModel_recipe(String publisher, String f2f_url, String title, String source_url, String recipe_id, String image_url, String social_rank, String publisher_url) {
         this.publisher = publisher;
@@ -23,66 +42,125 @@ public class DataModel_recipe {
         this.publisher_url = publisher_url;
 
     }
+
+    /**
+     * getter get publisher
+     * @return publisher
+     */
         public String getPublisher () {
             return publisher;
         }
 
+    /**
+     * setter set publisher
+     *
+     */
         public void setPublisher (String publisher){
             this.publisher = publisher;
         }
 
+    /**
+     * getter get f2f_url
+     * @return f2f_url
+     */
         public String getF2f_url () {
             return f2f_url;
         }
-
+    /**
+     * setter set f2f_url
+     *
+     */
         public void setF2f_url (String f2f_url){
             this.f2f_url = f2f_url;
         }
 
+    /**
+     * getter get title
+     * @return title
+     */
         public String getTitle () {
             return title;
         }
 
+    /**
+     * setter set title
+     *
+     */
         public void setTitle (String title){
             this.title = title;
         }
 
+    /**
+     * getter get source_url
+     * @return source_url
+     */
         public String getSource_url () {
             return source_url;
         }
-
+    /**
+     * setter set source_url
+     *
+     */
         public void setSource_url (String source_url){
             this.source_url = source_url;
         }
 
+    /**
+     * getter get recipe_id
+     * @return recipe_id
+     */
         public String getRecipe_id () {
             return recipe_id;
         }
 
+    /**
+     * setter set recipe_id
+     *
+     */
         public void setRecipe_id (String recipe_id){
             this.recipe_id = recipe_id;
         }
 
+    /**
+     * getter get image_url
+     * @return image_url
+     */
         public String getImage_url () {
             return image_url;
         }
-
+    /**
+     * setter set social_rank
+     *
+     */
         public void setImage_url (String image_url){
             this.image_url = image_url;
         }
-
+    /**
+     * getter get social_rank
+     * @return social_rank
+     */
         public String getSocial_rank () {
             return social_rank;
         }
-
+    /**
+     * setter set social_rank
+     *
+     */
         public void setSocial_rank (String social_rank){
             this.social_rank = social_rank;
         }
 
+    /**
+     * getter get publisher_url
+     * @return publisher_url
+     */
         public String getPublisher_url () {
             return publisher_url;
         }
-
+    /**
+     * setter set publisher_url
+     *
+     */
         public void setPublisher_url (String publisher_url){
             this.publisher_url = publisher_url;
         }
