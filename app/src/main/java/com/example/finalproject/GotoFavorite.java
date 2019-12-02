b6ef77852dda0066709370ddcd087ae13895f64a
Mingjiong Liao <liao0019@algonquinlive.com>
2019-12-01 21:02:32 -0500

    Update GotoFavorite.java
    add comments

--- app/src/main/java/com/example/finalproject/GotoFavorite.java
+++ app/src/main/java/com/example/finalproject/GotoFavorite.java
@@ -35,35 +35,6 @@ public class GotoFavorite extends AppCompatActivity {
         boolean isTablet = findViewById(R.id.favoriteFragment) != null;
         //query all the results from the database:
         viewData();
-        /*String[] columns = {FavoriteDBhelper.COL_ID, FavoriteDBhelper.COL_TITLE};
-        Cursor results = dbOpener.query(false, FavoriteDBhelper.TABLE_NAME, columns, null, null, null, null, null, null);
-
-        //find the column indices:
-        int titleIndex = results.getColumnIndex(FavoriteDBhelper.COL_TITLE);
-        int idColIndex = results.getColumnIndex(FavoriteDBhelper.COL_ID);
-        int latitudeColIndex = results.getColumnIndex(FavoriteDBhelper.COL_LATITUDE);
-        int longtitudeColIndex = results.getColumnIndex(FavoriteDBhelper.COL_LONGTITUDE);
-        int phoneColIndex = results.getColumnIndex(FavoriteDBhelper.COL_PHONE);
-
-        theList = (ListView) findViewById(R.id.the_list);
-        //iterate over the results, return true if there is a next item:
-        while (results.moveToNext()) {
-            String title = results.getString(titleIndex);
-            long id = results.getLong(idColIndex);
-
-            //add the new Contact to the array list:
-            stationList.add(new Station(title, id));
-        }
-
-        //create an adapter object and send it to the listVIew
-        Station [] arr = new Station[stationList.size()];
-        stationList.toArray(arr);
-
-        theList = findViewById(R.id.listview);
-        ArrayAdapter<Station> adapter = new ArrayAdapter<Station>(this,
-                android.R.layout.simple_expandable_list_item_1, arr);
-        theList.setAdapter(adapter);*/
-
         //This listens for items being clicked in the list view
         theList.setOnItemClickListener((parent, view, position, id) -> {
             Bundle dataToPass = new Bundle();
@@ -82,7 +53,7 @@ public class GotoFavorite extends AppCompatActivity {
                         .add(R.id.favoriteFragment, fFragment) //Add the fragment in FrameLayout
                         .addToBackStack("AnyName") //make the back button undo the transaction
                         .commit(); //actually load the fragment.
-            }else {
+            }else { //if on a phone, start a new page/intent
                 Intent emptyActivity = new Intent(this, Empty_favorite_fragment.class);
                 emptyActivity.putExtras(dataToPass);
                 startActivityForResult(emptyActivity, 345);
@@ -97,10 +68,12 @@ public class GotoFavorite extends AppCompatActivity {
 
         if (cursor.getCount() != 0){
             while (cursor.moveToNext()){
+                //use cursor to locate the data in the database table
                 Station model = new Station( cursor.getString(1),cursor.getLong(0));
                 model.setLatitude(cursor.getDouble(2));
                 model.setLongtitude(cursor.getDouble(3));
                 model.setPhone(cursor.getString(4));
+                //populate in the listview
                 stationList.add(model);
                 StationAdapter adt = new StationAdapter(stationList, getApplicationContext());
                 theList.setAdapter(adt);
@@ -126,7 +99,7 @@ public class GotoFavorite extends AppCompatActivity {
 
     public void deleteStationId(int id)
     {
-
+// dete by the id
         dbOpener.deleteEntry(id);
         stationList.clear();
         viewData();
